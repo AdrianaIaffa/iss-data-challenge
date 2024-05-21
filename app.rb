9726b2e01e42fb1696a55855ca0b3c5e52d52b93
AdrianaIaffa <adrianaiaffa@hotmail.com>
2024-05-21 15:59:26 +0100

    endpoint for iss json format data added

--- app.rb
+++ app.rb
@@ -41,3 +41,33 @@ get '/astronauts' do
   erb :astronauts, locals: {astronauts: astronauts}
 
 end
+
+get '/iss_position.json' do
+  #fetch ISS position data
+  iss_data = OpenNotify.iss_now
+
+  #assing the latitude and longitude to a variable
+  #like issData.iss_position.latitude but ruby is with []
+  # latitude = iss_data['iss_position']['latitude']
+  # longitude = iss_data['iss_position']['longitude']
+
+  #create json response
+  #i can actually reuse the iss_data and chage the time stamp
+  json_response = {
+    # "iss_position": {
+    #   "latitude": latitude,
+    #   "longitude": longitude
+    # },
+    "iss_position": iss_data["iss_position"],
+    #time.now is a class method in ruby, returns a time object
+    #.to_i converts time to an integer, seconds that have passed since 1970 lol
+    #Unix epoch format timestamp is standard
+    #opennotify said to use unix_time_stamp
+    "timestamp": Time.now.to_i,  
+    "message": "success"
+  }
+  #this is like headers in javascript
+  content_type :json
+  json_response.to_json
+
+end
